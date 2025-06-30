6ee5e899160edb0d19b90f85b1487bbf88df5542
Ondřej Budai <ondrej@budai.cz>
2025-06-30 23:44:08 +0200

    test: fix the tests on macos
    For this we need to properly cross-compile the orches executable for
    linux. Also, for some reason, netns=bridge doesn't work in podman
    desktop on linux, so we need to use netns=host, and thus cannot use
    PublishPort. Let's use --listen instead. It's a big ugly, but it works.

--- pkg/utils/exec.go
+++ pkg/utils/exec.go
@@ -2,23 +2,44 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
-func ExecNoOutput(argv ...string) error {
-	_, err := ExecOutput(argv...)
-	return err
-}
+// execCommand is the base function that handles command execution
+func execCommand(env []string, argv ...string) ([]byte, error) {
+	if len(argv) == 0 {
+		return nil, fmt.Errorf("no command provided")
+	}
 
-func ExecOutput(argv ...string) ([]byte, error) {
-	cmd := argv[0]
-	args := argv[1:]
-	c := exec.Command(cmd, args...)
+	cmd := exec.Command(argv[0], argv[1:]...)
+	if len(env) > 0 {
+		cmd.Env = append(os.Environ(), env...)
+	}
 
-	// TODO: use Output?
-	out, err := c.CombinedOutput()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return out, fmt.Errorf("failed to execute command: %w\noutput:\n%s", err, string(out))
 	}
 	return out, nil
+}
+
+func ExecNoOutput(argv ...string) error {
+	_, err := execCommand(nil, argv...)
+	return err
+}
+
+func ExecOutput(argv ...string) ([]byte, error) {
+	return execCommand(nil, argv...)
+}
+
+// ExecOutputEnv executes a command with additional environment variables and returns its output
+func ExecOutputEnv(env []string, argv ...string) ([]byte, error) {
+	return execCommand(env, argv...)
+}
+
+// ExecNoOutputEnv executes a command with additional environment variables
+func ExecNoOutputEnv(env []string, argv ...string) error {
+	_, err := execCommand(env, argv...)
+	return err
 }
